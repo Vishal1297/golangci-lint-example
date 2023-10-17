742cc937d50b8ce09b687f4906df98b2f9b5fc75
Vishal Yadav <vishalyadav1746@gmail.com>
2023-10-18 00:45:55 +0530

    Fix golangci workflow file

---
+++ examples/second-example.go
@@ -0,0 +1,30 @@
+package examples
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// This is the function we'll run in every goroutine.
+func worker(id int) {
+	fmt.Printf("Worker %d starting\n", id)
+
+	// Sleep to simulate an expensive task.
+	time.Sleep(time.Second)
+	fmt.Printf("Worker %d done\n", id)
+}
+
+func start() {
+	var wg sync.WaitGroup
+	for i := 1; i <= 5; i++ {
+		wg.Add(1)
+		i := i
+		go func() {
+			defer wg.Done()
+			worker(i)
+		}()
+	}
+
+	wg.Wait()
+}
